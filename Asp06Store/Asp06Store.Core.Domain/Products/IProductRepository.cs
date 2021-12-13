66c3071c1af5841071ae416429c054f6ef76e1a0
Mohammadreza <mreza.khansari@gmail.com>
2021-12-13 10:28:02 +0330

    commit first framework

---
+++ Asp06Store/Asp06Store.Core.Domain/Products/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Asp06Store.ShopUI.Models
+{
+    public interface IProductRepository
+    {
+        PageData<Product> GetAll(int pageNumber, int pageSize,string category);
+        
+        Product GetById(int id);
+    }
+}
---
+++ Asp06Store/Asp06Store.Framework/Paginations/PageData.cs
@@ -0,0 +1,9 @@
+﻿namespace Asp06Store.ShopUI.Models
+{
+    public class PageData<T>
+    {
+        public List<T> Data { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+
+}
